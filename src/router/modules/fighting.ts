870ca1842d7c6a1d042ad0f9455e6d4a3188f9f6
Cyrus <somprmis@gmail.com>
2025-06-20 09:15:11 +0800

    v0.1

---
+++ src/router/modules/fighting.ts
@@ -0,0 +1,17 @@
+// 最简代码，也就是这些字段必须有
+export default {
+  path: "/fighting",
+  meta: {
+    title: "加油"
+  },
+  children: [
+    {
+      path: "/fighting/index",
+      name: "Fighting",
+      component: () => import("@/views/fighting/index.vue"),
+      meta: {
+        title: "加油"
+      }
+    }
+  ]
+};
